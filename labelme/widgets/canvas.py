efb91cc76ccefb62c88373b2339a8a9944b5126c
snow-fox <taijunjet@hotmail.com>
2023-04-25 14:01:19 +0100

    fix git bugs

--- labelme/widgets/canvas.py
+++ labelme/widgets/canvas.py
@@ -542,11 +542,7 @@ class Canvas(QtWidgets.QWidget):
         y1 = top - point.y()
         x2 = right - point.x()
         y2 = bottom - point.y()
-<<<<<<< HEAD
-        self.offsets = QtCore.QPoint(int(x1), int(y1)), QtCore.QPoint(int(x2), int(y2))
-=======
         self.offsets = QtCore.QPointF(x1, y1), QtCore.QPointF(x2, y2)
->>>>>>> wkentaro_main
 
     def boundedMoveVertex(self, pos):
         index, shape = self.hVertex, self.hShape
@@ -563,15 +559,9 @@ class Canvas(QtWidgets.QWidget):
             pos -= QtCore.QPointF(min(0, o1.x()), min(0, o1.y()))
         o2 = pos + self.offsets[1]
         if self.outOfPixmap(o2):
-<<<<<<< HEAD
-            pos += QtCore.QPoint(
-                int(min(0), self.pixmap.width() - o2.x()),
-                int(min(0), self.pixmap.height() - o2.y()),
-=======
             pos += QtCore.QPointF(
                 min(0, self.pixmap.width() - o2.x()),
                 min(0, self.pixmap.height() - o2.y()),
->>>>>>> wkentaro_main
             )
         # XXX: The next line tracks the new position of the cursor
         # relative to the shape, but also results in making it
@@ -623,11 +613,7 @@ class Canvas(QtWidgets.QWidget):
         # Try to move in one direction, and if it fails in another.
         # Give up if both fail.
         point = shapes[0][0]
-<<<<<<< HEAD
-        offset = QtCore.QPoint(2, 2)
-=======
         offset = QtCore.QPointF(2.0, 2.0)
->>>>>>> wkentaro_main
         self.offsets = QtCore.QPoint(), QtCore.QPoint()
         self.prevPoint = point
         if not self.boundedMoveShapes(shapes, point - offset):
@@ -707,11 +693,7 @@ class Canvas(QtWidgets.QWidget):
         aw, ah = area.width(), area.height()
         x = (aw - w) / (2 * s) if aw > w else 0
         y = (ah - h) / (2 * s) if ah > h else 0
-<<<<<<< HEAD
         return QtCore.QPoint(int(x), int(y))
-=======
-        return QtCore.QPointF(x, y)
->>>>>>> wkentaro_main
 
     def outOfPixmap(self, p):
         w, h = self.pixmap.width(), self.pixmap.height()
@@ -755,17 +737,10 @@ class Canvas(QtWidgets.QWidget):
         if (x, y) == (x1, y1):
             # Handle cases where previous point is on one of the edges.
             if x3 == x4:
-<<<<<<< HEAD
-                return QtCore.QPoint(int(x3), int(min(max(0, y2), max(y3, y4))))
-            else:  # y3 == y4
-                return QtCore.QPoint(int(min(max(0, x2), max(x3, x4))), int(y3))
-        return QtCore.QPoint(int(x), int(y))
-=======
                 return QtCore.QPointF(x3, min(max(0, y2), max(y3, y4)))
             else:  # y3 == y4
                 return QtCore.QPointF(min(max(0, x2), max(x3, x4)), y3)
         return QtCore.QPointF(x, y)
->>>>>>> wkentaro_main
 
     def intersectingEdges(self, point1, point2, points):
         """Find intersecting edges.
@@ -792,13 +767,8 @@ class Canvas(QtWidgets.QWidget):
             if 0 <= ua <= 1 and 0 <= ub <= 1:
                 x = x1 + ua * (x2 - x1)
                 y = y1 + ua * (y2 - y1)
-<<<<<<< HEAD
-                m = QtCore.QPoint(int((x3 + x4) / 2), int((y3 + y4) / 2))
-                d = labelme.utils.distance(m - QtCore.QPoint(int(x2), int(y2)))
-=======
                 m = QtCore.QPointF((x3 + x4) / 2, (y3 + y4) / 2)
                 d = labelme.utils.distance(m - QtCore.QPointF(x2, y2))
->>>>>>> wkentaro_main
                 yield d, i, (x, y)
 
     # These two, along with a call to adjustSize are required for the
